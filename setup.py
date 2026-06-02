be8b3f41e015358f4f1634fcd69295ed650e08ce
Suraj Saithwar <159919245+SurajSaithwar18@users.noreply.github.com>
2026-06-02 22:14:33 +0530

    Update Setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(file_path: str) -> List[str]:
     return requirements
 
 setup(
-    name='ml_project',
+    name='Customer CHurn Prediction Using Machine Learning',
     version='0.0.1',
     author='Suraj',
     author_email='saithwarsuraj18@gmail.com',
